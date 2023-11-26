686f6f325155f3f79dac682bbf00f4af813ad667
Harshpandey9156 <Harshpandey21012@gmail.com>
2023-11-26 17:20:11 +0530

    comprmisition

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -27,3 +27,34 @@ let SomeNumber=33
 let StringNumber =String(SomeNumber)
 console.log(StringNumber)
 console.log(typeof StringNumber)
+
+
+
+//Operations are
+
+let value=3
+let negValue=-value
+console.log(negValue)
+
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true)
+console.log(+"")
