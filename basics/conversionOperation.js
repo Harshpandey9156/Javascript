d315fc15a81a6aaa724bab8951c89b332c3b6ff8
Harshpandey9156 <Harshpandey21012@gmail.com>
2023-11-26 17:00:30 +0530

    conversion

---
+++ basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score="33abc"
+
+
+console.log(typeof score)
+console.log(typeof (score))   
+//convertin the string in to the number
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let islogedIn= 1
+let booleanIsLoggedIn = Boolean(islogedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let SomeNumber=33
+
+let StringNumber =String(SomeNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
