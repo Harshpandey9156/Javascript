e989ad0dcf611aaeaf433f968ee4b3574e71a3e4
Harshpandey9156 <Harshpandey21012@gmail.com>
2023-12-03 01:18:53 +0530

    arrow

--- basic3/arrow.js
+++ basic3/arrow.js
@@ -3,7 +3,7 @@ const abc={
     price: 35,
     welcommessage: function(){
         console.log(`${this.username} Welcome to our website.`);
-        console.log(this)
+        // console.log(this)
     }
 
 
@@ -15,7 +15,7 @@ console.log(abc.welcommessage())
 function chai()
 {
     let username ="harsh"
-    console.log(this)//is not working in the function  because it is not a object it only work in object
+    console.log(this.username)//is not working in the function  because it is not a object it only work in object
 }
 chai()
 
@@ -24,3 +24,15 @@ const chaii = ()=> {
     console.log(this.username)  
 }   
 chaii()
+
+const addtwo = (a,b) => {   
+    return a+b
+}
+console.log(addtwo(2,3))
+
+const addtwoo = (a,b) => a+b
+console.log(addtwoo(2,3))
+
+//object return in the arrow function
+const addtwooo = (a,b) => ({username:"harsh",age:23})
+console.log(addtwooo(2,3))
