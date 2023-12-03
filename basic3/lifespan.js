81810ec507a26820aca17c2f561f5f87efc93720
Harshpandey9156 <Harshpandey21012@gmail.com>
2023-12-03 14:25:51 +0530

    life

---
+++ basic3/lifespan.js
@@ -0,0 +1,8 @@
+//immediately Invoked Function Expression IIFE
+(function abc9(){
+    console.log("I am abc9");
+})();
+
+(function abcd10(){
+    console.log("I am abc10");
+})()  //to remove the pollution of global scope we use this function    //it is also called as self invoking function
