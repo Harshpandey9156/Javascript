dbb528f94942124b13a4691a45b43ecb41dd8bf4
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-01-09 22:37:33 +0530

    yes

--- basics/dates.js
+++ basics/dates.js
@@ -14,7 +14,7 @@ console.log(mytimestamp);
 
 // Dates
 
-let myDate = new Date()
+let myDate = new Date() 
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
--- basics/variable.js
+++ basics/variable.js
@@ -1,7 +1,7 @@
 const accountId = 14553
 let accountEmail = "Harsh"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+let accountCity = "Jaipur"
 let accountState;
 
 // accountId = 2 // not allowed
